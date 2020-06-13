d13167ab141af95a24c460eb4534d64c11f9d5be
lochan.nimje <lochannimje515@gmail.com>
2020-06-13 18:14:16 +0530

    commit new order

--- src/main/java/team12/order/OrderApplication.java
+++ src/main/java/team12/order/OrderApplication.java
@@ -10,7 +10,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.client.RestTemplateBuilder;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.web.client.RestTemplate;
@@ -18,7 +17,6 @@ import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 @ComponentScan
-@EnableDiscoveryClient
 public class OrderApplication {
 
 
